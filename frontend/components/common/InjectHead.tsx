08ad66a0fd8050763cbfb18b0de8a0c57f4e5ec4
RealOmerGal <Omergal156@gmail.com>
2022-10-15 04:47:37 +0300

    Added head tag configuration

---
+++ frontend/components/common/InjectHead.tsx
@@ -0,0 +1,27 @@
+import Head from "next/head";
+import React from "react";
+
+interface Props {
+  title?: string;
+}
+
+const InjectHead = ({ title }: Props) => {
+  return (
+    <Head>
+      <title>{title ?? "GitIn"} </title>
+      <meta
+        name="description"
+        content="Gitin - Bringing code together"
+        key="description"
+      />
+      <meta name="keywords" content="Github, Linkedin, Gitin" key="keywords" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+        key="viewport"
+      />
+    </Head>
+  );
+};
+
+export default InjectHead;
